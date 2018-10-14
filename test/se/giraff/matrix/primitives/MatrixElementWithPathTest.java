d8569ff54846939d03985da62b65b9ea34b95317
Mykhailo Kolchenko <mykhailo.kolchenko@proposalsfactory.com>
2018-10-14 21:50:38 +0200

    test: Add initial test for MatrixElementWithPath

--- test/se/giraff/matrix/primitives/MatrixElementWithPathTest.java
+++ test/se/giraff/matrix/primitives/MatrixElementWithPathTest.java
@@ -1,5 +1,31 @@
+package se.giraff.matrix.primitives;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatrixElementWithPathTest {
 
+    @Test
+    void testSetPaths() {
+        MatrixElementWithPath elementFirst = new MatrixElementWithPath(new MatrixElement(1), Coordinate.from(0, 0));
+        elementFirst.markAsInitPathElement();
+
+        MatrixElementWithPath elementSecond = new MatrixElementWithPath(new MatrixElement(1), Coordinate.from(0, 1));
+        elementSecond.setPaths(elementFirst);
+
+        assertEquals(elementFirst.getPaths().size(), elementSecond.getPaths().size());
+
+        elementFirst.getPaths().forEach(path -> {
+            assertTrue(path.contains(elementFirst.getCoordinate()));
+            assertFalse(path.contains(elementSecond.getCoordinate()));
+        });
+
+        elementSecond.getPaths().forEach(path -> {
+            assertTrue(path.contains(elementFirst.getCoordinate()));
+            assertTrue(path.contains(elementSecond.getCoordinate()));
+        });
+
+        assertEquals(elementFirst.getWeight() + elementSecond.getWeight(), elementSecond.getInterimWeight());
+    }
 }
